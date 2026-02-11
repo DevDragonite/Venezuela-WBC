5728f42e2790a693cbff3b1e0f4f4a50477b43c5
Developer Dragonite <helycamargo115@gmail.com>
2026-02-11 14:59:31 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Venezuela-WBC/', // REEMPLAZA CON EL NOMBRE EXACTO DE TU REPOSITORIO [cite: 2026-02-09]
 })
